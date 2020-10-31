52260b60d5962b1867ad30b39f49f621e8559ce5
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2020-10-31 21:09:36 +0530

    Maxwell

--- Maxwell Equation/1-D_pulse.c
+++ Maxwell Equation/1-D_pulse.c
@@ -3,6 +3,8 @@
 #include <math.h>
 
 int main(int argc, char const *argv[]) {
-
+  int a;
+  for(int i=0; i< 10; i++)
+    printf("%i\n", a);
   return 0;
 }
---
+++ Maxwell Equation/README.md
@@ -0,0 +1,2 @@
+#Program 1-D_pulse
+use -lm as math library is also involve.
