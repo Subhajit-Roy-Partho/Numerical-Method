0b548f38d2988ab4dbb41b57e2ff38af7d263a3e
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2020-10-31 21:35:46 +0530

    Maxwell

--- Maxwell Equation/1-D_pulse.c
+++ Maxwell Equation/1-D_pulse.c
@@ -3,8 +3,24 @@
 #include <math.h>
 
 int main(int argc, char const *argv[]) {
-  int a;
-  for(int i=0; i< 10; i++)
-    printf("%i\n", a);
+  int cells = 200,kc=(cells/2),steps=100,t0=40,t;
+  double E[cells], B[cells],pulse=0,fh=12.0,a=1;
+
+
+  for(int i=0; i<cells;i++)
+    E[cells]= (double)0.0;
+    B[cells]= (double)0.0;
+
+
+  for(int n=0; n < steps; n++)
+    pulse = a*exp(-0.5 * (pow((t0 - t) / fh, 2.0)));
+    t+=1;
+    for(int i=1; i< cells-1; i++)
+      E[i]-= 0.5*(B[i-1]-B[i]);
+    for(int i=1; i<cells-1;i++)
+      B[i] -= 0.5*(B[i]-B[i+1]);
+
+
+
   return 0;
 }
--- Maxwell Equation/README.md
+++ Maxwell Equation/README.md
@@ -1,2 +1,7 @@
-#Program 1-D_pulse
+# Program 1-D_pulse
 use -lm as math library is also involve.
+
+
+#### Variables
+- kc = region for the start of the pulse
+- cells = number of computational unit.
