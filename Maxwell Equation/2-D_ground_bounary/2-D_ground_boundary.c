b83fe326fe298ab6f1ab6a7d3af0ea24c932615e
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2020-11-05 23:38:36 +0530

    update

--- Maxwell Equation/2-D_ground_bounary/2-D_ground_boundary.c
+++ Maxwell Equation/2-D_ground_bounary/2-D_ground_boundary.c
@@ -3,31 +3,61 @@
 #include <math.h>
 
 int main(int argc, char const *argv[]) {
-  int cells = 200,kc=(cells/2),steps=500;
-  double Ez[cells], Hx[cells],Hy[cells],pulse=0,fh=12.0,a=1.0,t0=40.0,t=0.0,Eold[2],Bold[2],factor;
+  int cells = 200,ic=(cells/2),jc=(cells/2),steps=2000;
+  double Ez[cells][cells], Hx[cells][cells],Hy[cells][cells],pulse=0,fh=12.0,a=1.0,t0=40.0,t=0.0,Eold[2],Bold[2],factor;
   FILE *f;
-  f = fopen("Outdata.dat","w");
+  char file[15]="dat.dat";
 
 
   for(int i=0; i<cells;i++){
-    Ez[i]= 0.0;
-    Hx[i]= 0.0;
-    Hy[i]= 0.0;
+    for(int j=0; j<cells; j++){
+      Ez[i][j]= 0.0;
+      Hx[i][j]= 0.0;
+      Hy[i][j]= 0.0;
+    }
   }
 
 
   for(int n=0; n < steps; n++){
+
     pulse = a*exp(-0.5 * (pow((t0 - t) / fh, 2.0)));
-    for(int j=0; j<cells-1;j++){
-      for()
+
+    sprintf(file,"Data/out%d.dat",n+1);
+    f = fopen(file,"w");
+
+
+    for(int j=1; j<cells;j++){
+      for(int i=1; i <cells; i++){
+        Ez[i][j] += 0.5*(Hy[i][j] - Hy[i-1][j] - Hx[i][j]+Hx[i][j-1]);
+      }
     }
+
+    Ez[ic][jc] += pulse;
+
+    for(int j=0; j< cells-1; j++){
+      for(int i=0; i< cells-1; i++){
+        Hx[i][j] += 0.5*(Ez[i][j]-Ez[i][j+1]);
+      }
+    }
+
+    for(int j=0; j < cells-1;j++){
+      for(int i=0; i< cells-1; i++){
+        Hy[i][j] += 0.5*(Ez[i+1][j]-Ez[i][j]);
+      }
+    }
+    for(int j=0; j<cells;j++){
+      for(int i=0; i < cells; i++){
+        fprintf(f, "%lf ",Ez[i][j]);
+      }
+      fprintf(f, "\n");
+    }
+    fflush(f);
+    fclose(f);
   }
-  fclose(f);
   f = fopen("gnuE.txt","w");
-  fprintf(f, "set term gif animate delay 3 optimize\nset output 'E.gif'\nset xrange[1:%i]\nset yrange[%lf:%lf]\n",cells,-(a+0.2),(a+0.2));
-  fprintf(f, "do for [i=0:%i]{\nplot 'Outdata.dat' every ::(((i-1)*200)+1)::(i*200) u 1:2 w l\n}\nexit",steps-1);
+  fprintf(f, "set hidden3d\nset pm3d\nset isosample 11\nset sample 11\nset term gif large animate delay 3 optimize\nset output 'out.gif'\nset print '-'\ndo for [i=1:%d]{\nsplot 'Data/out'.i.'.dat' matrix w l\n print i\n}\nexit",steps);
+  fflush(f);
   fclose(f);
   system("gnuplot gnuE.txt");
-  fflush(f);
   return 0;
 }
